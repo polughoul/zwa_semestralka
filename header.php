f8e54e86510d50eccae3dba0de900cfee26b9fee
kiz0r <kkhomenko10@gmail.com>
2023-10-21 23:20:57 +0200

    Add reset.css and fix styles and semantics for header.php

--- header.php
+++ header.php
@@ -1,15 +1,22 @@
-<header>
-    <nav>
-        <ul class="right_nav">
-            <a href="index.php">Home</a>
+ <header class="header">
+      <a href="index.php">MindfulCare</a>
+      <nav class="nav">
+        <ul class="navList">
+          <li>
             <a href="login.php">Log in</a>
+          </li>
+          <li>
             <a href="signin.php">Sign in</a>
+          </li>
+          <li>
             <a href="specialists.php">Our specialists</a>
+          </li>
+          <li>
             <a href="feedback.php">Leave a feedback</a>
+          </li>
+          <li>
             <a href="appointment.php">Make an appointment</a>
+          </li>
         </ul>
-        <ul class="left_nav">
-            <a href="index.php">MindfulCare</a>
-        </ul>
-    </nav>
+      </nav>
 </header>
